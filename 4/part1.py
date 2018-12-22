c8a077cecb24f9970218a9111437ae7a1b286b6f
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2018-12-22 00:06:21 +0000

    Worked more on 4

--- 4/part1.py
+++ 4/part1.py
@@ -1,22 +1,22 @@
 #!/usr/env python3
-from datetime import datetime, timedelta
+from datetime import datetime
 from operator import itemgetter
+from common import multiReplace
 
 
 def formatData(dataStr):
-    timestamp = datetime(*[int(i) for i in dataStr[1:17].replace("-", " ").replace(":", " ").split(" ")])
+    timestamp = datetime(*[int(i) for i in multiReplace(dataStr[1:17], "-:", " ").split(" ")])
+    activityStr = dataStr[19:]
     try:
-        activity = int(dataStr[19:].split(" ")[1].replace("#", ""))
+        activity = int(activityStr.split(" ")[1].replace("#", ""))
     except Exception:
-        activity = dataStr[19:]
+        activity = activityStr
     return (timestamp, activity)
 
 
-# Scrape the data from the file
+# Scrape the data from the file, format it and sort it
 with open("data.txt") as dataFile:
-    lines = [line.replace("\n", "") for line in dataFile.readlines()]
-data = [formatData(line) for line in lines]
-# Sort the data by date
+    data = [formatData(line.replace("\n", "")) for line in dataFile.readlines()]
 data.sort(key=itemgetter(0))
 
 # Generate the dictionary of guards
@@ -29,13 +29,18 @@ for point in data:
         except KeyError:
             guards[currentGuard] = [list()]
     else:
-        guards[currentGuard][-1].append(point)
+        guards[currentGuard][-1].append(point[0])
 
-# Change the dictionary so each guard has a list of a list of tuples each showing (sleeptime, waketime)
+# Change the dictionary so each guard has a list of days each of which is a list of timestamps
 for guardID, info in guards.items():
-    print(info)
     newInfo = list()
     for day in info:
+        daySeq = list()
+        sleepTime = 
+        for event in day:
+            daySeq.append(event)
         for i in range(0, len(day), 2):
-            newInfo.append([(info[i][0], info[i + 1][0]))
+
+        newInfo.append(daySeq)
+
     guards[guardID] = newInfo
