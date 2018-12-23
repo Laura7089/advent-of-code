abdd134b529eab4c83c3865d5e8342e635a49d6d
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2018-12-23 14:28:57 +0000

    Switch 6 to parallel processing

--- 6/part1.py
+++ 6/part1.py
@@ -1,8 +1,12 @@
 #!/usr/env python3
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import cpu_count
+from collections import Counter
+cpuPool = ThreadPool(cpu_count())
 
 
 def nearestCoord(targetGrid, targetX, targetY):
-    # If it's called on a coord
+    # If it's called on a coord, just return that coord number
     if isinstance(targetGrid[targetX][targetY], int):
         return targetGrid[targetX][targetY]
 
@@ -10,15 +14,14 @@ def nearestCoord(targetGrid, targetX, targetY):
     layer = 0
     escape = False
     found = list()
+    nums = [1, 0, -1]
+
     # Main loop
     while not escape:
-        # Increment the layer and set our starting values
         layer += 1
-        toCheck = [i for i in range(layer, (-1 * layer) - 1, -1)]
-        toCheck.extend(list(range((-1 * layer) + 1, layer)))
         # Generate the coords
-        for i in range(layer * 4):
-            coords.append((toCheck[(i + layer) % len(toCheck)], toCheck[i]))
+        toCheck = nums + list(reversed(nums[1:-1]))
+        coords = cpuPool.map(lambda i: (toCheck[(i + layer) % len(toCheck)], toCheck[i]), range(layer * 4))
 
         # Check all the coords
         for coord in coords:
@@ -30,6 +33,9 @@ def nearestCoord(targetGrid, targetX, targetY):
             except IndexError:
                 continue
 
+        # Expand our numbers list by one at each end
+        nums = [nums[0] + 1] + nums + [nums[-1] - 1]
+
     # Return the correct string representing the closest coord (or . if there's multiple)
     if len(found) > 1:
         return "."
@@ -37,56 +43,34 @@ def nearestCoord(targetGrid, targetX, targetY):
         return str(found[0])
 
 
-def renderGrid(targetGrid):
-    gridStr = ""
-    for y in range(len(targetGrid[0]) - 1, -1, -1):
-        for x in range(len(targetGrid)):
-            gridStr += str(targetGrid[x][y]) + " "
-        gridStr += "\n"
-    return gridStr
-
-
 # Scrape the data from the file into a list of coordinates (corrected to be 0-based)
 with open("data.txt") as coordsFile:
-    coords = [[int(coord) - 1 for coord in line.replace("\n", "").replace(",", "").split(" ")] for line in coordsFile.readlines()]
+    coords = cpuPool.map(lambda line: [int(coord) - 1 for coord in line.replace("\n", "").replace(",", "").split(" ")], coordsFile.readlines())
 
 # Generate a list of each axis coordinate
 xCoords = [coord[0] for coord in coords]
 yCoords = [coord[1] for coord in coords]
-# Generate the grid we need to be as large as we need it to be and fill it with "."
-grid = [["." for y in range(max(yCoords) + 1)] for x in range(max(xCoords) + 1)]
 
+# Generate the grid we need to be as large as we need it to be and fill it with "."
+grid = cpuPool.map(lambda xPos: ["." for y in range(max(yCoords) + 1)], range(max(xCoords) + 1))
 # Place all the coords
-for i in range(len(coords)):
+for i in range(len(xCoords)):
     grid[xCoords[i]][yCoords[i]] = i
 
 # Generate all the areas
 for x in range(len(grid)):
-    for y in range(len(grid[0])):
-        # print("Doing (" + str(x) + ", " + str(y) + ")")
-        grid[x][y] = nearestCoord(grid, x, y)
+    print("Generating line #" + str(x) + " of " + str(len(grid)))
+    grid[x] = cpuPool.map(lambda y: nearestCoord(grid, x, y), range(len(grid[0])))
 
 # Count up the areas, marking which ones contact the outside borders
-areas = dict()
-invalid = list()
+areas = {num: 0 for num in list(range(len(xCoords)))}
 for x in range(len(grid)):
-    for y in range(len(grid[0])):
-        currentVal = str(grid[x][y])
-        if currentVal != ".":
-            if x == len(grid) - 1 or y == len(grid[0]) - 1:
-                invalid.append(currentVal)
-            else:
-                try:
-                    areas[currentVal] += 1
-                except KeyError:
-                    areas[currentVal] = 1
-
+    for key, value in Counter(grid[x]).items():
+        areas[key] += value
+del areas["."]
 # Delete all the "bad" areas
-for bad in invalid:
-    try:
-        del(areas[bad])
-    except KeyError:
-        continue
+# for bad in invalid:
+    # del(areas[bad])
 
 # Print the largest area's size
 print(max(areas.keys()))
