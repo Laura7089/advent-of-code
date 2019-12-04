e43ef01dce72678ed7b62189e07f4ab924890435
Laura Demkowicz-Duffy <edward70891@gmail.com>
2019-12-04 16:47:53 +0000

    Add working solution to day 4 part 2

--- aoc2019py/day4/solver.py
+++ aoc2019py/day4/solver.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python3
+from collections import Counter
 
 
 def test_valid_pass(password):
+    # Check it's a 6-digit integer
     if not isinstance(password, int):
         return False
     password = str(password)
     if len(password) != 6:
         return False
+
+    # Check ordering and double-digit
+    double = False
     for i in range(len(password) - 1):
-        if password[i] == password[i + 1]:
-            break
+        if password[i] <= password[i + 1]:
+            if not double and password[i] == password[i + 1]:
+                double = True
+        else:
+            return False
+    if not double:
         return False
-    digits = list(map(int, password))
-    if sorted(digits) != digits:
-        return False
+
     return True
+
+
+def test_valid_strict(password):
+    if not test_valid_pass(password):
+        return False
+
+    counter = Counter(str(password))
+    if 2 in counter.values():
+        return True
+    return False
 
 
 if __name__ == "__main__":
@@ -28,3 +45,7 @@ if __name__ == "__main__":
     # print(f"{part1_valid=}")
     part1 = len(part1_valid)
     print(f"{part1=}")
+
+    part2_valid = list(filter(test_valid_strict, range(*wanted_range)))
+    part2 = len(part2_valid)
+    print(f"{part2=}")
--- aoc2019py/day4/test.py
+++ aoc2019py/day4/test.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from solver import test_valid_pass
+from solver import test_valid_pass, test_valid_strict
 
 
 class TestMe(unittest.TestCase):
@@ -10,6 +10,11 @@ class TestMe(unittest.TestCase):
         self.assertTrue(test_valid_pass(111111))
         self.assertFalse(test_valid_pass(223450))
         self.assertFalse(test_valid_pass(123789))
+
+    def test_part2(self):
+        self.assertTrue(test_valid_strict(112233))
+        self.assertTrue(test_valid_strict(111122))
+        self.assertFalse(test_valid_strict(123444))
 
 
 unittest.main()
