2a4f4ce01d80b98dac2b64c341a7bcc41526c5b4
Laura Demkowicz-Duffy <edward70891@gmail.com>
2019-12-06 16:22:42 +0000

    Add day6 solution (tests don't work yet)

---
+++ aoc2019/day6/test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from solver import make_tree, total_depth
+
+
+class TestMe(unittest.TestCase):
+    def test_part1(self):
+        data = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L""".split("\n")
+        _, trees = make_tree(data)
+        self.assertEqual(total_depth(trees), 42)
+
+    def test_part2(self):
+        data = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN""".split("\n")
+        _, trees = make_tree(data)
+        _, total = trees["YOU"].closest_root(trees["SAN"])
+        self.assertEqual(total, 4)
+
+
+unittest.main()
