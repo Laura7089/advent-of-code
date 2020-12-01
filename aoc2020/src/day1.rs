5aeeb4aaccb970c837bd1c52e179d7cb21293a57
Laura Demkowicz-Duffy <edward70891@gmail.com>
2020-12-01 10:45:09 +0000

    Day 1 part 2

--- aoc2020/src/day1.rs
+++ aoc2020/src/day1.rs
@@ -1,4 +1,4 @@
-const TARGET: u32 = 2020;
+const TOTAL_TARGET: u32 = 2020;
 
 #[aoc_generator(day1)]
 pub fn parse_input(input: &str) -> Vec<u32> {
@@ -9,11 +9,29 @@ pub fn parse_input(input: &str) -> Vec<u32> {
 }
 
 #[aoc(day1, part1)]
-fn solve_input(input: &[u32]) -> u32 {
+fn solve_input_part1(input: &[u32]) -> u32 {
+    let mut target;
+    for i in input {
+        target = TOTAL_TARGET - i;
+        for o in input {
+            if o == &target {
+                return i * o;
+            }
+        }
+    }
+    0
+}
+
+#[aoc(day1, part2)]
+fn solve_input_part2(input: &[u32]) -> u32 {
+    let mut target;
     for i in input {
         for o in input {
-            if i + o == TARGET {
-                return i + o;
+            target = TOTAL_TARGET - i - o;
+            for p in input {
+                if p == &target {
+                    return i * o * p;
+                }
             }
         }
     }
