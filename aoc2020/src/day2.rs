fb4b7315b1666c452d4f5632b0385727bcb05532
Laura Demkowicz-Duffy <edward70891@gmail.com>
2020-12-02 14:33:24 +0000

    Fix part 1, add part 2

--- aoc2020/src/day2.rs
+++ aoc2020/src/day2.rs
@@ -1,9 +1,9 @@
 #[derive(Debug, PartialEq)]
 pub struct PasswordWithPolicy {
-    pub letter: String,
-    pub password: String,
-    pub letter_min: u32,
-    pub letter_max: u32,
+    pub letter: Vec<u8>,
+    pub password: Vec<u8>,
+    pub num_first: u32,
+    pub num_second: u32,
 }
 
 #[aoc_generator(day2)]
@@ -15,24 +15,26 @@ pub fn parse_input(input: &str) -> Vec<PasswordWithPolicy> {
             let mut nums = line_iter.next().expect("Empty line!").split('-');
 
             PasswordWithPolicy {
-                letter_min: nums
+                num_first: nums
                     .next()
                     .expect("Bad number formatting")
                     .parse()
                     .expect("Bad number formatting"),
-                letter_max: nums
+                num_second: nums
                     .next()
                     .expect("Bad number formatting")
                     .parse()
                     .expect("Bad number formatting"),
-                letter: line_iter
-                    .next()
-                    .expect("Missing letter and password")
-                    .split(':')
-                    .next()
-                    .expect("Bad letter formatting")
-                    .to_string(),
-                password: line_iter.next().expect("Missing password").to_string(),
+                letter: Vec::from(
+                    line_iter
+                        .next()
+                        .expect("Missing letter and password")
+                        .split(':')
+                        .next()
+                        .expect("Bad letter formatting")
+                        .as_bytes(),
+                ),
+                password: Vec::from(line_iter.next().expect("Missing password").as_bytes()),
             }
         })
         .collect()
@@ -40,21 +42,31 @@ pub fn parse_input(input: &str) -> Vec<PasswordWithPolicy> {
 
 #[aoc(day2, part1)]
 pub fn solve_input_part1(input: &[PasswordWithPolicy]) -> u32 {
-    let mut total_valid = 0;
-    for password in input.iter() {
-        // TODO: Make compatible with multi-byte "letters"
-        let search_byte = password.letter.as_bytes()[0];
-        let password_bytes = password.password.as_bytes();
-        let mut letters_in_password = 0u32;
-        for letter in password_bytes.iter() {
-            letters_in_password += (letter == &search_byte) as u32;
-            if letters_in_password > password.letter_max {
-                continue;
-            }
+    input.iter().filter(valid_password_part1).count() as u32
+}
+
+fn valid_password_part1(password: &&PasswordWithPolicy) -> bool {
+    // TODO: Make compatible with multi-byte "letters"
+    let mut letters_in_password = 0u32;
+    for letter in password.password.iter() {
+        letters_in_password += (letter == &password.letter[0]) as u32;
+        if letters_in_password > password.num_second {
+            return false;
         }
-        total_valid += (letters_in_password >= password.letter_min) as u32;
     }
-    total_valid
+    letters_in_password >= password.num_first
+}
+
+#[aoc(day2, part2)]
+pub fn solve_input_part2(input: &[PasswordWithPolicy]) -> u32 {
+    input.iter().filter(valid_password_part2).count() as u32
+}
+
+fn valid_password_part2(password: &&PasswordWithPolicy) -> bool {
+    // TODO: Make compatible with multi-byte "letters"
+    let search_byte = password.letter[0];
+    (password.password[password.num_first as usize - 1] == search_byte)
+        ^ (password.password[password.num_second as usize - 1] == search_byte)
 }
 
 #[cfg(test)]
@@ -62,17 +74,17 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
-    #[test_case("1-3 a: abcde".to_string(), PasswordWithPolicy{ letter_min: 1, letter_max: 3, letter: "a".to_string(), password: "abcde".to_string() })]
-    #[test_case("1-3 b: cdefg".to_string(), PasswordWithPolicy{ letter_min: 1, letter_max: 3, letter: "b".to_string(), password: "cdefg".to_string() })]
-    #[test_case("2-9 c: ccccccccc".to_string(), PasswordWithPolicy{ letter_min: 2, letter_max: 9, letter: "c".to_string(), password: "ccccccccc".to_string() })]
+    #[test_case("1-3 a: abcde".to_string(), PasswordWithPolicy{ num_first: 1, num_second: 3, letter: "a".to_string(), password: "abcde".to_string() })]
+    #[test_case("1-3 b: cdefg".to_string(), PasswordWithPolicy{ num_first: 1, num_second: 3, letter: "b".to_string(), password: "cdefg".to_string() })]
+    #[test_case("2-9 c: ccccccccc".to_string(), PasswordWithPolicy{ num_first: 2, num_second: 9, letter: "c".to_string(), password: "ccccccccc".to_string() })]
     fn parser(input: String, expected: PasswordWithPolicy) {
         assert_eq!(parse_input(&input)[0], expected);
     }
 
-    #[test_case(&[PasswordWithPolicy{ letter_min: 1, letter_max: 3, letter: "a".to_string(), password: "abcde".to_string() }], 1)]
-    #[test_case(&[PasswordWithPolicy{ letter_min: 1, letter_max: 3, letter: "b".to_string(), password: "cdefg".to_string() }], 0)]
-    #[test_case(&[PasswordWithPolicy{ letter_min: 2, letter_max: 9, letter: "c".to_string(), password: "ccccccccc".to_string() }], 1)]
-    fn part1(input: &[PasswordWithPolicy], expected: u32) {
-        assert_eq!(solve_input_part1(input), expected);
+    #[test_case(PasswordWithPolicy{ num_first: 1, num_second: 3, letter: "a".to_string(), password: "abcde".to_string() }, true)]
+    #[test_case(PasswordWithPolicy{ num_first: 1, num_second: 3, letter: "b".to_string(), password: "cdefg".to_string() }, false)]
+    #[test_case(PasswordWithPolicy{ num_first: 2, num_second: 9, letter: "c".to_string(), password: "ccccccccc".to_string() }, true)]
+    fn part1(input: PasswordWithPolicy, expected: bool) {
+        assert_eq!(valid_password_part1(&&input), expected);
     }
 }
