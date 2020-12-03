644893ea029a856d1033feb3af8ef3feb8271eaa
Laura Demkowicz-Duffy <edward70891@gmail.com>
2020-12-03 14:21:24 +0000

    Allow custom input file running in main binary

--- aoc2020/src/main.rs
+++ aoc2020/src/main.rs
@@ -1,33 +1,36 @@
+use std::env;
 use std::fs;
 
 fn main() {
+    let input_dir = env::args().nth(1).unwrap_or("input/2020".to_string());
+
     println!(
         "Day 1 part 1: {}\nDay 1 part 2: {}\n",
         aoc2020::day1::solve_input_part1(&aoc2020::day1::parse_input(
-            &fs::read_to_string("input/2020/day1.txt").expect("No data found!")
+            &fs::read_to_string(format!("{}/day1.txt", input_dir)).expect("No data found!")
         )),
         aoc2020::day1::solve_input_part2(&aoc2020::day1::parse_input(
-            &fs::read_to_string("input/2020/day1.txt").expect("No data found!")
+            &fs::read_to_string(format!("{}/day1.txt", input_dir)).expect("No data found!")
         ))
     );
 
     println!(
         "Day 2 part 1: {}\nDay 2 part 2: {}\n",
         aoc2020::day2::solve_input_part1(&aoc2020::day2::parse_input(
-            &fs::read_to_string("input/2020/day2.txt").expect("No data found!")
+            &fs::read_to_string(format!("{}/day2.txt", input_dir)).expect("No data found!")
         )),
         aoc2020::day2::solve_input_part2(&aoc2020::day2::parse_input(
-            &fs::read_to_string("input/2020/day2.txt").expect("No data found!")
+            &fs::read_to_string(format!("{}/day2.txt", input_dir)).expect("No data found!")
         ))
     );
 
     println!(
         "Day 3 part 1: {}\nDay 3 part 2: {}\n",
         aoc2020::day3::solve_input_part1(&aoc2020::day3::get_slope(
-            &fs::read_to_string("input/2020/day3.txt").expect("No data found!")
+            &fs::read_to_string(format!("{}/day3.txt", input_dir)).expect("No data found!")
         )),
         aoc2020::day3::solve_input_part2(&aoc2020::day3::get_slope(
-            &fs::read_to_string("input/2020/day3.txt").expect("No data found!")
+            &fs::read_to_string(format!("{}/day3.txt", input_dir)).expect("No data found!")
         ))
     );
 }
