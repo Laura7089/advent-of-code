377c55e217a939227d66a8bf1d81c4bcb0db1f48
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2021-12-07 20:00:26 +0000

    Start answering 6i

--- aoc2021/src/day6.rs
+++ aoc2021/src/day6.rs
@@ -1,11 +1,55 @@
+const DAY_COUNT: usize = 18;
+const CYCLE_TIME: usize = 7;
+
 #[aoc_generator(day6)]
 fn parse_input(input: &str) -> Vec<usize> {
     input.split(",").map(|n| n.parse().unwrap()).collect()
 }
-
 #[aoc(day6, part1)]
 fn solve_part1(input: &[usize]) -> usize {
-    unimplemented!()
+    // let mut fish: Vec<usize> = Vec::from(input);
+    // let mut days_remaining = DAY_COUNT;
+    // let mut step = 0;
+
+    // println!("Start: {:?}", fish);
+
+    // while step <= days_remaining {
+    //     for i in 0..fish.len() {
+    //         if fish[i] == 0 {
+    //             fish.push(CYCLE_TIME + 1);
+    //             fish[i] = CYCLE_TIME - 1;
+    //         }
+    //     }
+
+    //     step = *fish.iter().min().unwrap();
+    //     for i in 0..fish.len() {
+    //         fish[i] -= step;
+    //     }
+    //     days_remaining -= step;
+    //     println!(
+    //         "Day {} (step {}): {:?}",
+    //         DAY_COUNT - days_remaining,
+    //         step,
+    //         fish
+    //     );
+    // }
+
+    // fish.len()
+
+    let shoal = Vec::from(input);
+    let mut total = 0;
+
+    for fish in shoal.iter() {
+        let num_spawns = (DAY_COUNT - fish + 1) / CYCLE_TIME;
+        println!(
+            "Fish {} will spawn {} times in the coming {} days",
+            fish, num_spawns, DAY_COUNT
+        );
+        let children = 2_usize.pow(num_spawns as u32);
+
+        total += children;
+    }
+    total
 }
 
 #[aoc(day6, part2)]
