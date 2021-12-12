2cf8efb70b6130af395eedaf598febd2fa457e5d
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2021-12-12 01:57:26 +0000

    Start to solve 8pt2, alter data structures slightly, add day 8 input

--- aoc2021/src/day8.rs
+++ aoc2021/src/day8.rs
@@ -1,7 +1,48 @@
-#[derive(Debug, Clone)]
+const OUTPUT_DIGITS: usize = 4;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct DigitCode {
+    segs: [bool; 7],
+}
+
+impl DigitCode {
+    fn new() -> Self {
+        Self { segs: [false; 7] }
+    }
+    fn from_str(input: &str) -> Self {
+        let mut segs = [false; 7];
+        input.bytes().for_each(|c| segs[c as usize - 97] = true);
+        Self { segs }
+    }
+
+    fn len(&self) -> usize {
+        self.segs.iter().filter(|e| **e).count()
+    }
+
+    fn is_subset(&self, other: &Self) -> bool {
+        for i in 0..7 {
+            if self.segs[i] && !other.segs[i] {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fn diff(&self, other: &Self) -> usize {
+        let mut count = 0;
+        for i in 0..7 {
+            if self.segs[i] != other.segs[i] {
+                count += 1;
+            }
+        }
+        count
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub struct InputLine {
-    digit_codes: [String; 10],
-    output: [String; 4],
+    digit_codes: [DigitCode; 10],
+    output: [DigitCode; OUTPUT_DIGITS],
 }
 
 #[aoc_generator(day8)]
@@ -15,17 +56,17 @@ fn parse_input(input: &str) -> Vec<InputLine> {
 
             InputLine {
                 digit_codes: digit_codes
-                    .split_whitespace()
-                    .map(str::to_string)
+                    .split(" ")
+                    .map(DigitCode::from_str)
                     .collect::<Vec<_>>()
                     .try_into()
-                    .unwrap(),
+                    .expect("Wrong number of unique codes"),
                 output: output_values
                     .split_whitespace()
-                    .map(str::to_string)
+                    .map(DigitCode::from_str)
                     .collect::<Vec<_>>()
                     .try_into()
-                    .unwrap(),
+                    .expect("Wrong number of output digits"),
             }
         })
         .collect()
@@ -33,21 +74,122 @@ fn parse_input(input: &str) -> Vec<InputLine> {
 
 #[aoc(day8, part1)]
 fn solve_part1(input: &[InputLine]) -> usize {
-    let input: Vec<InputLine> = dbg!(input.to_vec());
+    let input: Vec<InputLine> = input.to_vec();
     input
         .into_iter()
         .map(|i| i.output.into_iter())
         .flatten()
         .filter(|elem| {
-            let length = dbg!(elem.len());
+            let length = elem.len();
             length == 2 || length == 4 || length == 3 || length == 7
         })
         .count()
 }
 
 #[aoc(day8, part2)]
-fn solve_part2(_input: &[InputLine]) -> usize {
-    unimplemented!()
+fn solve_part2(input: &[InputLine]) -> usize {
+    let input: Vec<InputLine> = input.to_vec();
+    input
+        .into_iter()
+        .map(|line| {
+            let mut digits = [DigitCode::new(); 10];
+            // Unpack input struct
+            let InputLine {
+                digit_codes,
+                output,
+            } = line;
+            let mut digit_codes: Vec<DigitCode> = digit_codes.to_vec();
+
+            // We use a 3-step method to match the digits
+            // We pop them as we match them
+            // Each step depends on the matches from the last
+            // After each step, we check that we got them all
+
+            // Step 1: "Easy digits"
+            for i in (0..digit_codes.len()).rev() {
+                match digit_codes[i].len() {
+                    2 => digits[1] = digit_codes.remove(i),
+                    3 => digits[7] = digit_codes.remove(i),
+                    4 => digits[4] = digit_codes.remove(i),
+                    7 => digits[8] = digit_codes.remove(i),
+                    _ => (),
+                }
+            }
+
+            assert_eq!(digit_codes.len(), 6);
+
+            // Step 2: Digits based on easy digits
+            for i in (0..digit_codes.len()).rev() {
+                match digit_codes[i].len() {
+                    5 => {
+                        // Whichever one is a superset of digits[1] is 3
+                        if digits[1].is_subset(&digit_codes[i]) {
+                            digits[3] = digit_codes.remove(i);
+                            continue;
+                        }
+                    }
+                    6 => {
+                        // 1 diff with digits[1] = 6
+                        if digits[1].diff(&digit_codes[i]) == 1 {
+                            digits[6] = digit_codes.remove(i);
+                            continue;
+                        }
+                        // Superset of digits[7] = 9
+                        if digits[7].is_subset(&digit_codes[i]) {
+                            digits[9] = digit_codes.remove(i);
+                            continue;
+                        }
+                    }
+                    _ => panic!(),
+                }
+            }
+
+            assert_eq!(digit_codes.len(), 3);
+
+            // Step 3: The stragglers
+            for i in (0..digit_codes.len()).rev() {
+                match digit_codes[i].len() {
+                    // Either 2 or 5
+                    5 => {
+                        if digit_codes[i].is_subset(&digits[9]) {
+                            // 5 is a subset of 9
+                            digits[5] = digit_codes.remove(i);
+                            continue;
+                        } else {
+                            // It can only be 2
+                            digits[2] = digit_codes.remove(i);
+                            continue;
+                        }
+                    }
+                    // Only 6-length left is 0
+                    6 => {
+                        digits[0] = digit_codes.remove(i);
+                        continue;
+                    }
+                    _ => panic!(),
+                }
+            }
+
+            assert_eq!(digit_codes.len(), 0);
+
+            // Calculate the output number
+            output
+                .into_iter()
+                .enumerate()
+                .map(|(pos, od)| {
+                    // Poor way of matching into the digits array
+                    for (i, c) in digits.iter().enumerate() {
+                        if &od == c {
+                            // Multiply into correct position
+                            let mult = (10_usize).pow(3 - pos as u32);
+                            return i * mult;
+                        }
+                    }
+                    panic!("No matching digit found");
+                })
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -73,13 +215,13 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
     #[test]
     fn part2_example() {
-        assert_eq!(solve_part2(&parse_input(&EXAMPLE_INPUT)), unimplemented!());
+        assert_eq!(solve_part2(&parse_input(&EXAMPLE_INPUT)), 5353);
     }
 
     #[test]
     fn part1_myinput() {
         let input = crate::get_input_for_day(8);
-        assert_eq!(solve_part1(&parse_input(&input)), unimplemented!());
+        assert_eq!(solve_part1(&parse_input(&input)), 247);
     }
 
     #[test]
