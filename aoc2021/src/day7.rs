eb312cbc47412169fe80600c5301ed772a629919
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2021-12-12 00:11:04 +0000

    Add day 7 solutions (part 2 doesn't work)

--- aoc2021/src/day7.rs
+++ aoc2021/src/day7.rs
@@ -5,12 +5,39 @@ pub fn parse_input(input: &str) -> Vec<usize> {
 
 #[aoc(day7, part1)]
 pub fn solve_part1(input: &[usize]) -> usize {
-    unimplemented!()
+    let mut input: Vec<usize> = input.to_vec();
+    // Best position is in the median when sorted
+    input.sort();
+    let best_pos = input[input.len() / 2];
+
+    input
+        .into_iter()
+        .map(|pos| {
+            if best_pos > pos {
+                best_pos - pos
+            } else {
+                pos - best_pos
+            }
+        })
+        .sum()
 }
 
 #[aoc(day7, part2)]
 pub fn solve_part2(input: &[usize]) -> usize {
-    unimplemented!()
+    // Best position is the mean
+    let best_pos = (input.iter().sum::<usize>() as f64 / input.len() as f64).ceil() as usize;
+
+    input
+        .into_iter()
+        .map(|pos| {
+            let dist = if best_pos > *pos {
+                best_pos - pos
+            } else {
+                pos - best_pos
+            };
+            (dist * (dist + 1)) / 2
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -26,6 +53,12 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(solve_part2(&parse_input(EXAMPLE_INPUT)), unimplemented!());
+        assert_eq!(solve_part2(&parse_input(EXAMPLE_INPUT)), 168);
+    }
+
+    #[test]
+    fn part1_myinput() {
+        let input = crate::get_input_for_day(7);
+        assert_eq!(solve_part1(&parse_input(&input)), 336040);
     }
 }
