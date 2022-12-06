048d1af9549dbfc958492f1afda02b94d61f50ce
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-12-06 13:54:33 +0000

    feat(2022): do day 6 part 2

--- aoc2022/src/day6.rs
+++ aoc2022/src/day6.rs
@@ -1,22 +1,17 @@
 use itertools::Itertools;
 
-const PACKET_LEN: usize = 4;
-
-#[aoc(day6, part1)]
-fn solve_part1(input: &str) -> usize {
+fn find_unique<const PACKET_LEN: usize>(input: &str) -> usize {
     let mut window = [' '; PACKET_LEN];
     input
         .chars()
         .take(PACKET_LEN)
         .enumerate()
         .for_each(|(i, c)| window[i] = c);
-    println!("{:?}", window);
 
     let mut ptr = 0;
     for (i, c) in input.chars().enumerate().skip(PACKET_LEN) {
         window[ptr] = c;
         ptr = (ptr + 1) % PACKET_LEN;
-        println!("{:?}", window);
 
         if window.iter().unique().count() == PACKET_LEN {
             return i + 1;
@@ -25,27 +20,46 @@ fn solve_part1(input: &str) -> usize {
     panic!("No valid sequence found!");
 }
 
+#[aoc(day6, part1)]
+fn solve_part1(input: &str) -> usize {
+    find_unique::<4>(input)
+}
+
 #[aoc(day6, part2)]
 fn solve_part2(input: &str) -> usize {
-    todo!()
+    find_unique::<14>(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const SAMPLE_INPUT: &'static [(&'static str, usize)] = &[
-        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    use crate::get_input;
+    const SAMPLE_INPUT: &'static [(&'static str, usize, usize)] = &[
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26),
     ];
 
     #[test]
     fn part1_example() {
-        for (input, expected) in SAMPLE_INPUT {
+        for (input, expected, _) in SAMPLE_INPUT {
             println!("Trying '{input}'");
             assert_eq!(solve_part1(input), *expected);
+        }
+    }
+
+    #[test]
+    fn part1_mine() {
+        assert_eq!(solve_part1(&get_input(6)), 1760);
+    }
+
+    #[test]
+    fn part2_example() {
+        for (input, _, expected) in SAMPLE_INPUT {
+            println!("Trying '{input}'");
+            assert_eq!(solve_part2(input), *expected);
         }
     }
 }
