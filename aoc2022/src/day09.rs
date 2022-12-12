406deadc205807338cf515fcfcf78b9cc332ddb9
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-12-12 20:42:24 +0000

    feat(2022): add day 9 part 2

--- aoc2022/src/day09.rs
+++ aoc2022/src/day09.rs
@@ -6,7 +6,7 @@ enum Move {
     Hor(isize),
 }
 
-#[derive(Clone, PartialEq, Debug, Hash, Eq)]
+#[derive(Copy, Clone, PartialEq, Debug, Hash, Eq)]
 struct Knot(isize, isize);
 
 impl Knot {
@@ -19,19 +19,19 @@ impl Knot {
             (0, -2) => self.1 -= 1,
 
             // Multi-dimension movements
-            (1, 2) | (2, 1) => {
+            (1, 2) | (2, 1) | (2, 2) => {
                 self.0 += 1;
                 self.1 += 1;
             }
-            (-1, 2) | (-2, 1) => {
+            (-1, 2) | (-2, 1) | (-2, 2) => {
                 self.0 -= 1;
                 self.1 += 1;
             }
-            (1, -2) | (2, -1) => {
+            (1, -2) | (2, -1) | (2, -2) => {
                 self.0 += 1;
                 self.1 -= 1;
             }
-            (-1, -2) | (-2, -1) => {
+            (-1, -2) | (-2, -1) | (-2, -2) => {
                 self.0 -= 1;
                 self.1 -= 1;
             }
@@ -43,7 +43,6 @@ impl Knot {
 
 impl Move {
     fn apply(&mut self, knot: &mut Knot) -> bool {
-        println!("{self:?}");
         match self {
             Self::Ver(dy) => {
                 knot.1 += dy.signum();
@@ -78,8 +77,7 @@ fn generate(input: &str) -> Vec<Move> {
         .collect()
 }
 
-#[aoc(day09, part1)]
-fn solve_part1(input: &[Move]) -> usize {
+fn run_knot_snake<const LENGTH: usize>(input: &[Move]) -> usize {
     // TODO: hashing is SLOOOOOOW
     // Allocate enough for 50% of total move units to produce a unique coord
     let mut tail_positions: HashSet<Knot> = HashSet::with_capacity(
@@ -93,26 +91,32 @@ fn solve_part1(input: &[Move]) -> usize {
             / 2,
     );
 
-    let mut head = Knot(0, 0);
-    let mut tail = Knot(0, 0);
+    let mut knots = [Knot(0, 0); LENGTH];
 
     for mov in input {
         let mut m = mov.clone();
         let mut cont = true;
         while cont {
-            cont = !m.apply(&mut head);
-            tail.follow(&head);
-            println!("{head:?}, {tail:?}");
-            tail_positions.insert(tail.clone());
+            cont = !m.apply(&mut knots[0]);
+            for i in 1..LENGTH {
+                let this_head = knots[i - 1];
+                knots[i].follow(&this_head);
+            }
+            tail_positions.insert(knots[LENGTH - 1].clone());
         }
     }
 
     tail_positions.len()
 }
 
+#[aoc(day09, part1)]
+fn solve_part1(input: &[Move]) -> usize {
+    run_knot_snake::<2>(input)
+}
+
 #[aoc(day09, part2)]
 fn solve_part2(input: &[Move]) -> usize {
-    todo!()
+    run_knot_snake::<10>(input)
 }
 
 #[cfg(test)]
@@ -139,12 +143,29 @@ R 2";
     }
 
     #[test]
-    fn part2_example() {
-        assert_eq!(solve_part2(&generate(SAMPLE_INPUT)), 36);
+    fn part2_example1() {
+        assert_eq!(solve_part2(&generate(SAMPLE_INPUT)), 1);
+    }
+
+    #[test]
+    fn part2_example2() {
+        assert_eq!(
+            solve_part2(&generate(
+                "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"
+            )),
+            36
+        );
     }
 
     #[test]
     fn part2_mine() {
-        assert_eq!(solve_part2(&generate(&crate::get_input(09))), todo!());
+        assert_eq!(solve_part2(&generate(&crate::get_input(09))), 2327);
     }
 }
