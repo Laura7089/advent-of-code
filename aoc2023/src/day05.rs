b00b233d924d81a077552131c8f37903143c8978
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2023-12-05 21:25:54 +0000

    feat(2023d5): naive solution for part 2

--- aoc2023/src/day05.rs
+++ aoc2023/src/day05.rs
@@ -52,14 +52,14 @@ impl<K> Map<K> {
     }
 }
 
-fn parse_input(input: &str) -> (Vec<usize>, Vec<Map<usize>>) {
-    let mut iter = input.split("\n\n");
-    let seeds: Vec<usize> = iter.next().expect("empty input")[7..]
+fn parse_seeds(line: &str) -> impl Iterator<Item = usize> + '_ {
+    line[7..]
         .split(" ")
         .map(|v| v.parse().expect("bad integer literal"))
-        .collect();
+}
 
-    let maps: Vec<_> = iter.map(Map::from).collect();
+fn parse_input<'a>(input: impl IntoIterator<Item = &'a str>) -> Vec<Map<usize>> {
+    let maps: Vec<_> = input.into_iter().map(Map::from).collect();
     let mut keys: Vec<_> = maps
         .iter()
         .map(|m| m.source)
@@ -68,48 +68,60 @@ fn parse_input(input: &str) -> (Vec<usize>, Vec<Map<usize>>) {
     keys.insert(0, "seed");
     keys.push("location");
 
-    (
-        seeds,
-        maps.into_iter()
-            .map(|m| Map {
-                source: keys.iter().position(|k| k == &m.source).unwrap(),
-                dest: keys.iter().position(|k| k == &m.dest).unwrap(),
-                ranges: m.ranges,
-            })
-            .collect::<Vec<_>>(),
-    )
+    let mut maps: Vec<_> = maps
+        .into_iter()
+        .map(|m| Map {
+            source: keys.iter().position(|k| k == &m.source).unwrap(),
+            dest: keys.iter().position(|k| k == &m.dest).unwrap(),
+            ranges: m.ranges,
+        })
+        .collect();
+
+    maps.sort_by_key(|m| m.source);
+    maps
+}
+
+fn follow_through(maps: &[Map<usize>], seed: usize) -> usize {
+    let mut cur = maps[0].lookup(seed);
+    let mut dest = maps[0].dest;
+    for _ in 1..maps.len() {
+        cur = maps[dest].lookup(cur);
+        dest = maps[dest].dest;
+    }
+    cur
 }
 
 #[aoc(day05, part1)]
 fn solve_part1(input: &str) -> usize {
-    let (seeds, mut maps) = parse_input(input);
-    maps.sort_by_key(|m| m.source);
+    let mut iter = input.split("\n\n");
+    let seeds = parse_seeds(&iter.next().expect("empty input"));
+    let maps = parse_input(iter);
 
     seeds
-        .into_iter()
-        .map(|s| {
-            let mut cur = maps[0].lookup(s);
-            let mut dest = maps[0].dest;
-            for _ in 1..maps.len() {
-                cur = maps[dest].lookup(cur);
-                dest = maps[dest].dest;
-            }
-            cur
-        })
+        .map(|s| follow_through(&maps, s))
         .min()
         .expect("no maps in input")
 }
 
 #[aoc(day05, part2)]
-fn solve_part2(_input: &str) -> usize {
-    todo!()
+fn solve_part2(input: &str) -> usize {
+    let mut iter = input.split("\n\n");
+    let seeds: Vec<_> = parse_seeds(&iter.next().expect("empty input")).collect();
+    let seeds = seeds.chunks(2).map(|c| (c[0], c[1]));
+
+    let maps = parse_input(iter);
+
+    seeds
+        .flat_map(|(start, len)| start..=(start + len))
+        .map(|s| follow_through(&maps, s))
+        .min()
+        .expect("no maps in input")
 }
 
 #[cfg(test)]
 mod tests {
     #![allow(unreachable_code)]
     use super::*;
-    use test_case::test_case;
 
     const SAMPLE_INPUT: &str = "seeds: 79 14 55 13
 
@@ -145,23 +157,6 @@ humidity-to-location map:
 60 56 37
 56 93 4";
 
-    // #[test_case(
-    //     "seed-to-soil map:\n50 98 2\n52 50 48",
-    //     "seed",
-    //     "soil",
-    //     &[[50, 98, 2], [52, 50, 48]],
-    // )]
-    fn map_parse(input: &str, source: &str, dest: &str, ranges: &[MapRange]) {
-        assert_eq!(
-            Map::from(input),
-            Map {
-                source,
-                dest,
-                ranges: ranges.to_vec(),
-            }
-        );
-    }
-
     mod part1 {
         use super::*;
 
@@ -181,10 +176,11 @@ humidity-to-location map:
 
         #[test]
         fn example() {
-            assert_eq!(solve_part2(SAMPLE_INPUT), todo!());
+            assert_eq!(solve_part2(SAMPLE_INPUT), 46);
         }
 
         #[test]
+        #[ignore]
         fn mine() {
             assert_eq!(solve_part2(&crate::get_input(05)), todo!());
         }
