6717cdfbffd8d3eb3ce106e19193a11b8ab94315
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2023-12-27 02:38:36 +0000

    feat(2023d12): write some bullshit that probably doesn't make sense

--- aoc2023/src/day12.rs
+++ aoc2023/src/day12.rs
@@ -7,6 +7,17 @@ enum SpringState {
 
 type Row = (Vec<SpringState>, Vec<usize>);
 
+fn parse_lit(raw: &str) -> Vec<SpringState> {
+    raw.bytes()
+        .map(|b| match b {
+            b'#' => SpringState::Damaged,
+            b'.' => SpringState::Operational,
+            b'?' => SpringState::Unknown,
+            _ => panic!("unknown spring char {}", b as char),
+        })
+        .collect()
+}
+
 #[aoc_generator(day12)]
 fn generate(input: &str) -> Vec<Row> {
     input
@@ -15,24 +26,74 @@ fn generate(input: &str) -> Vec<Row> {
             let (lit_raw, list_raw) = line.split_once(' ').expect("no space in line");
 
             (
-                lit_raw
-                    .bytes()
-                    .map(|b| match b {
-                        b'#' => SpringState::Damaged,
-                        b'.' => SpringState::Operational,
-                        b'?' => SpringState::Unknown,
-                        _ => panic!("unknown spring char {}", b as char),
-                    })
-                    .collect(),
+                parse_lit(lit_raw),
                 list_raw.split(',').map(|n| n.parse().unwrap()).collect(),
             )
         })
         .collect()
 }
 
+#[derive(Clone)]
+struct DamageIter<'a> {
+    plot: &'a [SpringState],
+    off: usize,
+    damage_len: usize,
+}
+
+impl<'a> DamageIter<'a> {
+    fn new(plot: &'a [SpringState], damage_len: usize) -> Self {
+        Self {
+            plot,
+            damage_len,
+            off: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for DamageIter<'a> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for (i, _) in self
+            .plot
+            .iter()
+            .enumerate()
+            .skip(self.off)
+            .filter(|(_, s)| s != &&SpringState::Operational)
+        {
+            // Ensure (possible) gap on left
+            if i != 0 && self.plot[i - 1] == SpringState::Damaged {
+                continue;
+            }
+            let end = i + self.damage_len;
+            // Ensure (possible) gap on right
+            if self.plot.get(end + 1) == Some(&SpringState::Damaged) {
+                continue;
+            }
+            // Ensure possible fit
+            if !self.plot[i..end]
+                .iter()
+                .all(|s| matches!(s, SpringState::Damaged | SpringState::Unknown))
+            {
+                continue;
+            }
+
+            self.off += i;
+            return Some(self.off);
+        }
+        None
+    }
+}
+
 #[aoc(day12, part1)]
-fn solve_part1(_input: &[Row]) -> usize {
-    todo!()
+fn solve_part1(input: &[Row]) -> usize {
+    input
+        .iter()
+        .map(|(plot, list)| {
+            let mut iter = DamageIter::new(plot, *list.first().unwrap());
+            todo!() as usize
+        })
+        .sum()
 }
 
 #[aoc(day12, part2)]
@@ -44,6 +105,7 @@ fn solve_part2(_input: &[Row]) -> usize {
 mod tests {
     #![allow(unreachable_code)]
     use super::*;
+    use test_case::test_case;
 
     const SAMPLE_INPUT: &str = "???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -51,6 +113,12 @@ mod tests {
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1";
+
+    #[test_case("???.###", 1 => vec![0, 1, 2])]
+    #[test_case("???.###", 3 => vec![0])]
+    fn test_damage_iter(seq: &str, len: usize) -> Vec<usize> {
+        DamageIter::new(&parse_lit(seq), len).collect()
+    }
 
     mod part1 {
         use super::*;
