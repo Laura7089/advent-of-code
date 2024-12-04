3998190f2997d4d24ce137c4e5f0999adf914b40
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-12-04 23:17:30 +0000

    feat(2024d4): part 2

--- aoc2024/src/day04.rs
+++ aoc2024/src/day04.rs
@@ -5,38 +5,76 @@ fn generate(input: &str) -> Vec<Vec<u8>> {
     input.lines().map(|line| line.as_bytes().to_vec()).collect()
 }
 
-const GOAL: &[u8] = b"XMAS";
+const P1_GOAL: &[u8] = b"XMAS";
 
 #[inline]
-fn get_lower_and_modifier(modi: isize) -> (usize, isize) {
+fn get_limits(modi: isize, dim: usize, goal: &[u8]) -> std::ops::Range<usize> {
     // invert the modifier because limits need to encroach opposite
     // the direction of movement
     let modi = modi * -1;
 
-    let n0 = (GOAL.len() - 1) * modi.clamp(0, 1) as usize;
-    let n1m = (GOAL.len() - 1) as isize * modi.clamp(-1, 0);
+    let n0 = (goal.len() - 1) * modi.clamp(0, 1) as usize;
+    let n1m = (goal.len() - 1) as isize * modi.clamp(-1, 0);
+    let n1 = dim.saturating_add_signed(n1m);
 
-    (n0, n1m)
+    n0..n1
 }
 
-fn count_direction(ymod: isize, xmod: isize, input: &[Vec<u8>]) -> usize {
+#[inline]
+fn try_read_2way(
+    input: &[Vec<u8>],
+    goal: &[u8],
+    x: usize,
+    y: usize,
+    xmod: isize,
+    ymod: isize,
+) -> bool {
+    let glen = goal.len() as isize;
+
+    // search forwards
+    'forwards: {
+        for index in 0isize..glen {
+            let y_ = y.saturating_add_signed(ymod * index);
+            let x_ = x.saturating_add_signed(xmod * index);
+            if input[y_][x_] != goal[index as usize] {
+                break 'forwards;
+            }
+        }
+        // all letters matched :)
+        return true;
+    }
+
+    // search backwards
+
+    // first, offset our starting point
+    let y = y.saturating_add_signed((glen - 1) * ymod);
+    let x = x.saturating_add_signed((glen - 1) * xmod);
+
+    for index in 0isize..glen {
+        // then, work back through the offset (ie. note the -1)
+        let y_ = y.saturating_add_signed(ymod * index * -1);
+        let x_ = x.saturating_add_signed(xmod * index * -1);
+        if input[y_][x_] != goal[index as usize] {
+            return false;
+        }
+    }
+    true
+}
+
+fn count_aligned(ymod: isize, xmod: isize, input: &[Vec<u8>]) -> usize {
     let mut acc = 0;
 
-    let (y0, y1m) = get_lower_and_modifier(ymod);
-    let y1 = input.len().saturating_add_signed(y1m);
-    let (x0, x1m) = get_lower_and_modifier(xmod);
-    let x1 = input[0].len().saturating_add_signed(x1m);
+    let yrange = get_limits(ymod, input.len(), P1_GOAL);
+    let xrange = get_limits(xmod, input[0].len(), P1_GOAL);
 
-    for y in y0..y1 {
-        'search: for x in x0..x1 {
-            for index in 0isize..(GOAL.len() as isize) {
-                let y_ = y.saturating_add_signed(ymod * index);
-                let x_ = x.saturating_add_signed(xmod * index);
-                if input[y_][x_] != GOAL[index as usize] {
-                    continue 'search;
-                }
+    for y in yrange {
+        // why is Range not Copy.....
+        //              \/\/\/
+        for x in xrange.clone() {
+            // NOTE: this will break if the search term is palindromic
+            if try_read_2way(input, P1_GOAL, x, y, xmod, ymod) {
+                acc += 1;
             }
-            acc += 1;
         }
     }
     acc
@@ -45,30 +83,43 @@ fn count_direction(ymod: isize, xmod: isize, input: &[Vec<u8>]) -> usize {
 #[aoc(day04, part1)]
 fn solve_part1(input: &[Vec<u8>]) -> usize {
     [
-        // search down
-        count_direction(-1, 0, input),
-        // search left
-        count_direction(0, -1, input),
-        // search up
-        count_direction(1, 0, input),
-        // search right
-        count_direction(0, 1, input),
-        // search down-right
-        count_direction(-1, 1, input),
-        // search down-left
-        count_direction(-1, -1, input),
-        // search up-left
-        count_direction(1, -1, input),
-        // search up-right
-        count_direction(1, 1, input),
+        // search up and down
+        count_aligned(1, 0, input),
+        // search right and left
+        count_aligned(0, 1, input),
+        // search up-left and down-right
+        count_aligned(-1, 1, input),
+        // search up-right and down-left
+        count_aligned(-1, -1, input),
     ]
     .into_iter()
     .sum()
 }
 
+const P2_GOAL: &[u8] = b"MAS";
+
 #[aoc(day04, part2)]
 fn solve_part2(input: &[Vec<u8>]) -> usize {
-    todo!()
+    let (height, width) = (input.len(), input[0].len());
+    let mut count = 0;
+
+    for y in 0..(height - P2_GOAL.len() + 1) {
+        for x in 0..(width - P2_GOAL.len() + 1) {
+            // look up-right/down-left
+            if !try_read_2way(input, P2_GOAL, x, y, 1, 1) {
+                continue;
+            }
+
+            // offset x
+            let xm = x + P2_GOAL.len() - 1;
+            // then look up-left/down-right
+            if try_read_2way(input, P2_GOAL, xm, y, -1, 1) {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -106,12 +157,12 @@ MXMXAXMASX";
 
         #[test]
         fn example() {
-            assert_eq!(solve_part2(&generate(SAMPLE_INPUT)), todo!());
+            assert_eq!(solve_part2(&generate(SAMPLE_INPUT)), 9);
         }
 
         #[test]
         fn mine() {
-            assert_eq!(solve_part2(&generate(&crate::get_input(04))), todo!());
+            assert_eq!(solve_part2(&generate(&crate::get_input(04))), 1796);
         }
     }
 }
