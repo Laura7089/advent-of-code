f3fcbdc82e9919d05aa92bf21a34ecd8f9913ca4
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-12-05 14:58:59 +0000

    feat(2024d5): part 2

--- aoc2024/src/day05.rs
+++ aoc2024/src/day05.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 mod parse {
     use super::{PageOrdering, PageUpdate};
     use nom::{
@@ -52,32 +54,131 @@ fn generate(input: &str) -> (Vec<PageOrdering>, Vec<PageUpdate>) {
     parse::whole_input(input).expect("parse error").1
 }
 
+// note that irrelevant orderings are here considered to match
+fn ordering_matches_update(update: &PageUpdate, &(first, second): &PageOrdering) -> bool {
+    let Some(first_idx) = find_index(update, &first) else {
+        return true;
+    };
+    let Some(second_idx) = find_index(update, &second) else {
+        return true;
+    };
+
+    if first_idx > second_idx {
+        return false;
+    }
+
+    true
+}
+
+#[inline]
+fn is_update_valid(update: &PageUpdate, orderings: &[PageOrdering]) -> bool {
+    orderings
+        .iter()
+        .all(|ordering| ordering_matches_update(update, ordering))
+}
+
+#[inline]
+fn middle(update: &PageUpdate) -> usize {
+    update[update.len() / 2]
+}
+
 #[aoc(day05, part1)]
 fn solve_part1((orderings, updates): &(Vec<PageOrdering>, Vec<PageUpdate>)) -> usize {
     updates
         .iter()
-        .filter_map(|update| {
-            for &(first, second) in orderings {
-                let Some(first_idx) = find_index(update, &first) else {
-                    continue;
-                };
-                let Some(second_idx) = find_index(update, &second) else {
-                    continue;
-                };
-
-                if first_idx > second_idx {
-                    return None;
-                }
-            }
-
-            Some(update[update.len() / 2])
-        })
+        .filter_map(|update| is_update_valid(update, orderings).then(|| middle(update)))
         .sum()
 }
 
+#[derive(Ord, Eq)]
+struct ValWithOrdering<'a> {
+    ordering: &'a [PageOrdering],
+    value: usize,
+}
+
+impl<'a> PartialOrd for ValWithOrdering<'a> {
+    // assumes both sides refer to the same page ordering
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.ordering.contains(&(self.value, other.value)) {
+            Some(std::cmp::Ordering::Less)
+        } else if self.ordering.contains(&(other.value, self.value)) {
+            Some(std::cmp::Ordering::Greater)
+        } else {
+            // hopefully this is unreachable otherwise we're knackered
+            None
+        }
+    }
+}
+
+impl<'a> PartialEq for ValWithOrdering<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
 #[aoc(day05, part2)]
-fn solve_part2(input: &(Vec<PageOrdering>, Vec<PageUpdate>)) -> usize {
-    todo!()
+fn solve_part2((orderings, updates): &(Vec<PageOrdering>, Vec<PageUpdate>)) -> usize {
+    let mut mismatch_buf = Vec::new();
+    let mut update_buf = Vec::new();
+
+    let mut total = 0;
+
+    for update in updates {
+        mismatch_buf.clear();
+
+        for ordering in orderings {
+            if !ordering_matches_update(update, ordering) {
+                mismatch_buf.push(*ordering);
+            }
+        }
+        if mismatch_buf.len() == 0 {
+            // all the orderings matched
+            continue;
+        }
+
+        update_buf.clear();
+        update_buf.extend_from_slice(update);
+
+        // To do this, we need to sort the relevant numbers within the indexes they
+        // currently have. Nothing else needs to move - therefore, if none of them
+        // are in the middle, then we needn't bother >:)
+        // We need to make assumptions of the imput:
+        // - orderings are together *total*
+        // - updates contain no duplicate pages
+
+        // determine indexes of important elements
+        // note BTreeSets always iterate in order
+        let mut indexes: BTreeSet<_> = mismatch_buf
+            .iter()
+            // flatten the orderings into a list of numbers for now
+            .flat_map(|(l, r)| [l, r].into_iter())
+            .map(|n| find_index(update, n).unwrap())
+            .collect();
+
+        if !indexes.contains(&(update_buf.len() / 2)) {
+            // the middle element isn't affected by the ordering, so don't bother
+            total += middle(&update_buf);
+            continue;
+        }
+
+        let mut values: Vec<_> = indexes
+            .iter()
+            .map(|&i| ValWithOrdering {
+                ordering: &mismatch_buf,
+                value: update_buf[i],
+            })
+            .collect();
+        values.sort_unstable();
+
+        // TODO: replace with select_nth_unstable
+        for (i, ValWithOrdering { value: new_val, .. }) in indexes.into_iter().zip(values) {
+            update_buf[i] = new_val;
+        }
+
+        total += middle(&update_buf);
+    }
+
+    total
 }
 
 #[cfg(test)]
@@ -133,12 +234,12 @@ mod tests {
 
         #[test]
         fn example() {
-            assert_eq!(solve_part2(&generate(SAMPLE_INPUT)), todo!());
+            assert_eq!(solve_part2(&generate(SAMPLE_INPUT)), 123);
         }
 
         #[test]
         fn mine() {
-            assert_eq!(solve_part2(&generate(&crate::get_input(05))), todo!());
+            assert_eq!(solve_part2(&generate(&crate::get_input(05))), 6204);
         }
     }
 }
